a499bcfdf0d7149ed5d3c8bb7cfee935ef07dce8
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-02-06 14:19:24 +0300

    60

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -8,74 +8,48 @@
 
 bool vertificationNumber(int[] savedNumbers, int check)
         {
-            int count = 0;
             for (int i = 0; i < savedNumbers.Length; i++)
             {
                 if(savedNumbers[i] == check)
                 {
-                    count ++;
+                    return false;
                 }
             }
-            if(count > 1) return false;
-            else return true;
+            return true;
         }
 
-        Console.Write("Enter x: ");
-        int x = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter y: ");
-        int y = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter z: ");
-        int z = Convert.ToInt32(Console.ReadLine());
-        int[,,] array60 = new int[x, y, z];
-        int[] savedNums = new int[x*y*z];
+Console.Write("Enter x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] array60 = new int[x, y, z];
+int[] savedNums = new int[x*y*z];
 
-        for (int i = 0; i < array60.GetLength(0); i++)
+for (int i = 0; i < array60.GetLength(0); i++)
+{
+    for (int j = 0; j < array60.GetLength(1); j++)
+    {
+        for (int k = 0; k < array60.GetLength(2); k++)
         {
-            for (int j = 0; j < array60.GetLength(1); j++)
+            array60[i, j, k] = new Random().Next(10, 100);
+
+            while(!vertificationNumber(savedNums, array60[i, j, k]))
             {
-                for (int k = 0; k < array60.GetLength(2); k++)
-                {
-                    array60[i, j, k] = new Random().Next(10, 100);
-
-                    int num = array60[i, j, k];
-
-                    for (int m = 0; m < savedNums.Length; m++)
-                    {
-                        if(savedNums[m] != num && savedNums[m] == 0) 
-                        {
-                            savedNums[m] = num;
-                            break;
-                        }
-                    }
-                    
-                    bool isOk = vertificationNumber(savedNums, num);
-
-                    if(isOk == false)
-                    {
-                        while(isOk == false)
-                        {
-                            isOk = true;
-                            array60[i, j, k] = new Random().Next(10, 100);
-                            num = array60[i, j, k];
-                            for (int m = 0; m < savedNums.Length; m++)
-                            {
-                                if(savedNums[m] != num && savedNums[m] == 0)
-                                {
-                                        savedNums[m] = num;
-                                        break;
-                                }
-                                else
-                                {
-                                    isOk = false;
-                                    break;
-                                }
-                            }
-                            if(isOk == true) vertificationNumber(savedNums, num);
-                        }
-                    }
-
-                    Console.Write(array60[i, j, k] + $"({i}, {j}, {k}) ");
-                }
-            Console.WriteLine();
+                array60[i, j, k] = new Random().Next(10, 100);
             }
+
+            for (int m = 0; m < savedNums.Length; m++)
+            {
+                if(savedNums[m] == 0) 
+                {
+                    savedNums[m] = array60[i, j, k];
+                    break;
+                }
+            }
+            Console.Write(array60[i, j, k] + $"({i}, {j}, {k}) ");
         }
+    Console.WriteLine();
+    }
+}
